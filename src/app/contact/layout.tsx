c4eba8c7909ed8e12c24c6158cf56e8993e94d4f
crokerda111-maker <crokerda111@gmail.com>
2026-02-06 11:34:27 +0900

    525512

--- src/app/contact/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "문의 | BLUEDEAL",
-  description: "문의 게시판",
-};
-
-export default function ContactLayout({ children }: { children: React.ReactNode }) {
-  return <div className="space-y-6">{children}</div>;
-}
